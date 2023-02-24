a53aeba2e398d1b82f7cdf5369743e0b5a617f5e
yungzeeongit <greyw4300@gmail.com>
2023-02-23 17:27:41 -0800

    starting script

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 
 let board = document.getElementById('board');
 let redpieces = document.querySelectorAll('.redpawn')
-
-
-
-
+const greenpieces = document.querySelectorAll('.greenpawn')
+const yellowpieces = document.querySelectorAll('.yellowpawn')
+const bluepieces = document.querySelectorAll('.bluepawn')
 
 // let greenpiece1 = document.getElementByClass('quad2-pawn1')
 // let greenpiece2 = document.getElementByClass('quad2-pawn2')
@@ -28,7 +27,17 @@ redpieces.forEach((redpiece) => {
     redpiece.addEventListener('click', startRedPiece)
 })
 
+greenpieces.forEach((greenpiece) =>{
+    greenpiece.addEventListener('click', startGreenPiece)
+})
 
+yellowpieces.forEach((yellowpiece) => {
+    yellowpiece.addEventListener('click', startYellowPiece)
+})
+
+bluepieces.forEach((bluepiece) =>{
+    bluepiece.addEventListener('click', startBluePiece)
+})
 /* functions */
 
 function startRedPiece(piece) {
@@ -39,4 +48,31 @@ function startRedPiece(piece) {
        quadrant.removeChild(gamePiece)
        redStart.appendChild(gamePiece)
 
+}
+
+function startGreenPiece(piece) {
+    let greenStart = document.getElementById('green-start');
+    let gamePiece = piece.target
+    let quadrant = gamePiece.parentNode
+
+    quadrant.removeChild(gamePiece)
+    greenStart.appendChild(gamePiece)
+}
+
+function startYellowPiece(piece) {
+    let yellowStart = document.getElementById('yellow-start');
+    let gamePiece = piece.target
+    let quadrant = gamePiece.parentNode
+
+    quadrant.removeChild(gamePiece)
+    yellowStart.appendChild(gamePiece)
+}
+
+function startBluePiece(piece) {
+    let blueStart = document.getElementById('blue-start')
+    let gamepiece = piece.target
+    let quadrant = gamepiece.parentNode
+
+    quadrant.removeChild(gamepiece)
+    blueStart.appendChild(gamepiece)
 }
